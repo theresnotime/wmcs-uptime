f25a1fb1480d3097b2256ff6867cfef1c99fcd1e
TheresNoTime <sam@theresnotime.co.uk>
2022-06-02 16:24:13 +0100

    Database work

--- db/patch-unified-wmcs.sql
+++ db/patch-unified-wmcs.sql
@@ -1,6 +1,7 @@
 -- Unified selection of database patches
-
 BEGIN TRANSACTION;
+
+--
 CREATE TABLE setting_dg_tmp
 (
     id INTEGER
@@ -14,32 +15,22 @@ CREATE TABLE setting_dg_tmp
 INSERT INTO setting_dg_tmp(id, key, value, type) SELECT id, key, value, type FROM setting;
 DROP TABLE setting;
 ALTER TABLE setting_dg_tmp RENAME TO setting;
-COMMIT;
 
-
-BEGIN TRANSACTION;
--- For sendHeartbeatList
+--
 CREATE INDEX monitor_time_index ON heartbeat (monitor_id, time);
--- For sendImportantHeartbeatList
 CREATE INDEX monitor_important_time_index ON heartbeat (monitor_id, important,time);
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 ALTER TABLE user
     ADD twofa_secret VARCHAR(64);
 ALTER TABLE user
     ADD twofa_status BOOLEAN default 0 NOT NULL;
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 ALTER TABLE monitor
     ADD retry_interval INTEGER default 0 not null;
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 CREATE TABLE incident
 (
     id                INTEGER      not null
@@ -53,10 +44,8 @@ CREATE TABLE incident
     pin               BOOLEAN                default 1 not null,
     active            BOOLEAN                default 1 not null
 );
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 CREATE TABLE `group`
 (
     id           INTEGER      not null
@@ -79,32 +68,24 @@ CREATE INDEX [fk]
     ON [monitor_group] (
                         [monitor_id],
                         [group_id]);
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 ALTER TABLE monitor
     ADD push_token VARCHAR(20) DEFAULT NULL;
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 ALTER TABLE monitor
     ADD method TEXT default 'GET' not null;
 ALTER TABLE monitor
     ADD body TEXT default null;
 ALTER TABLE monitor
     ADD headers TEXT default null;
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 ALTER TABLE user
     ADD twofa_last_token VARCHAR(6);
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 CREATE TABLE [notification_sent_history] (
     [id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     [type] VARCHAR(50) NOT NULL,
@@ -117,18 +98,14 @@ CREATE INDEX [good_index] ON [notification_sent_history] (
     [monitor_id],
     [days]
 );
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 ALTER TABLE monitor
     ADD basic_auth_user TEXT default null;
 ALTER TABLE monitor
     ADD basic_auth_pass TEXT default null;
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 CREATE TABLE [status_page](
     [id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     [slug] VARCHAR(255) NOT NULL UNIQUE,
@@ -151,10 +128,8 @@ CREATE TABLE [status_page_cname](
 );
 ALTER TABLE incident ADD status_page_id INTEGER;
 ALTER TABLE [group] ADD status_page_id INTEGER;
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 CREATE TABLE proxy (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     user_id INT NOT NULL,
@@ -171,25 +146,20 @@ CREATE TABLE proxy (
 ALTER TABLE monitor ADD COLUMN proxy_id INTEGER REFERENCES proxy(id);
 CREATE INDEX proxy_id ON monitor (proxy_id);
 CREATE INDEX proxy_user_id ON proxy (user_id);
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 ALTER TABLE monitor
     ADD expiry_notification BOOLEAN default 1;
-COMMIT;
 
-BEGIN TRANSACTION;
+--
 ALTER TABLE status_page
     ADD footer_text TEXT;
 ALTER TABLE status_page
     ADD custom_css TEXT;
 ALTER TABLE status_page
     ADD show_powered_by BOOLEAN NOT NULL DEFAULT 1;
-COMMIT;
 
-
-BEGIN TRANSACTION;
+--
 ALTER TABLE monitor
     ADD mqtt_topic TEXT;
 ALTER TABLE monitor
@@ -198,4 +168,5 @@ ALTER TABLE monitor
     ADD mqtt_username VARCHAR(255);
 ALTER TABLE monitor
     ADD mqtt_password VARCHAR(255);
-COMMIT;
+
+END TRANSACTION;
